babbe0603cea7102a2db006fa5481a1f5fe50d8a
Welyson <welysonsr@gmail.com>
2022-03-15 15:13:08 -0300

    Novas resoluçoes para forma didáticas

--- 1º Fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia-4-javascript-es6-higher-order-functions-reduce/script1.js
+++ 1º Fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia-4-javascript-es6-higher-order-functions-reduce/script1.js
@@ -12,4 +12,18 @@ const flatten = (arr) => {
     return acc;
   }, [])
 }
+
+const flatten2 = (arr) => {
+  return arr.reduce((acc, crr) => {
+    crr.reduce((accumulator, interaction) => acc.push(interaction), 0)
+    return acc;
+  }, [])
+}
+
+const flatten3 = (arr) => {
+  return arr.reduce((acc, crr) => acc.concat(crr), [])
+}
+
 console.log(flatten(arrays));
+console.log(flatten2(arrays));
+console.log(flatten3(arrays));
