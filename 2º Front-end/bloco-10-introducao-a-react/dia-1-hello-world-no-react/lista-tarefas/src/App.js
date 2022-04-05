3dc1f34221a3b6867efb879613e6b7b55d94abfe
Welyson <welysonsr@gmail.com>
2022-04-05 11:36:45 -0300

    inserindo a key

--- 2º Front-end/bloco-10-introducao-a-react/dia-1-hello-world-no-react/lista-tarefas/src/App.js
+++ 2º Front-end/bloco-10-introducao-a-react/dia-1-hello-world-no-react/lista-tarefas/src/App.js
@@ -1,14 +1,14 @@
 import './App.css';
 
-const Task = (value) => {
+const Task = (value, index) => {
   return (
-    <li>{value}</li>
+    <li key={index}>{value}</li>
   );
 }
 const compromissos = ['Domingo', 'Segunda-feira', 'Terça-feira', 'Quarta-feira', 'Quinta-feira', 'Sexta-feira', 'Sábado'];
 function App() {
   return (
-    compromissos.map((compromisso) => Task(compromisso))
+    <ul>{compromissos.map((compromisso, index) => Task(compromisso, index))}</ul>
   );
 }
 
