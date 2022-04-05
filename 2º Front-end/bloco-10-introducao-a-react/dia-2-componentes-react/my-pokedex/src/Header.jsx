9612d7ba16f02e63e59c9da196683e364c529176
Welyson <welysonsr@gmail.com>
2022-04-05 20:10:00 -0300

    exercicio 10.2

---
+++ 2º Front-end/bloco-10-introducao-a-react/dia-2-componentes-react/my-pokedex/src/Header.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+class Header extends React.Component{
+  render(){
+    return <img className="header" src="https://assets.pokemon.com/assets/cms2-pt-br/img/misc/gus/buttons/logo-pokemon-79x45.png" alt="Pokedex" />
+  }
+}
+
+export default Header
